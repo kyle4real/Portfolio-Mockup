856807f2a57de3bc19382e448a97c70066ed4433
Kyle Friel <businessfriel@gmail.com>
2021-06-12 21:23:21 -0400

    Added swiper.js and created projects section

--- app.js
+++ app.js
@@ -8,3 +8,26 @@ $(document).on("scroll", () => {
 $(document).ready(() => {
     console.log("yes");
 });
+
+const swiper = new Swiper(".swiper-container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
