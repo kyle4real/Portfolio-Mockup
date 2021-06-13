f9776c9fcced4b0c245dff566e2c63ccb5c1497c
Kyle Friel <businessfriel@gmail.com>
2021-06-13 13:11:30 -0400

    restyled project cards and added hover effect for links

--- app.js
+++ app.js
@@ -9,6 +9,23 @@ $(document).ready(() => {
     console.log("yes");
 });
 
+const projectsHover = () => {
+    const projectsNum = 3;
+    for (let i = 0; i < projectsNum; i++) {
+        const current = $(`#proj-${i + 1}`)[0];
+        const currentBody = $(current).find("div")[0];
+        $(current).hover(
+            () => {
+                $(currentBody).removeClass("d-none");
+            },
+            () => {
+                $(currentBody).addClass("d-none");
+            }
+        );
+    }
+};
+projectsHover();
+
 const swiper = new Swiper(".swiper-container", {
     effect: "coverflow",
     grabCursor: true,
